07b72a3878bb751e377e2d13fc0617561510fd25
hum <65712791+hum@users.noreply.github.com>
2021-03-05 09:02:07 +0100

    add TODO comments

--- peep.go
+++ peep.go
@@ -38,33 +38,39 @@ func (w *Whois) Search(name string, servers ...string) (bool, error) {
 	}
 
 	for _, d := range w.Domains {
+		// TODO:
+		// Allow the input of a specific WHOIS server from param
 		if len(servers) == 0 || servers[0] == "" {
-			result, err := w.lookup(name+d, IANA_SERVER)
+			result, err := w.lookup(name+d, IANA_SERVER, time.Second*15)
 			if err != nil {
 				return false, err
 			}
+			// TODO:
+			// Parse text (internal.Parser) and find if it points to another WHOIS server
+			// If not, return; if yes, recursively search for the final one
 			fmt.Println(result)
 		}
 	}
 	return true, nil
 }
 
-func (w *Whois) lookup(name, server string) (string, error) {
-	conn, err := net.DialTimeout("tcp", net.JoinHostPort(server, DEFAULT_PORT), time.Second*15)
+func (w *Whois) lookup(name, server string, timeout time.Second) (string, error) {
+	conn, err := net.DialTimeout("tcp", net.JoinHostPort(server, DEFAULT_PORT), timeout)
 	if err != nil {
 		return "", err
 	}
 	defer conn.Close()
 
-	conn.SetWriteDeadline(time.Now().Add(time.Second * 15))
-
-	payload := []byte(name + "\r\n")
+	conn.SetWriteDeadline(time.Now().Add(timeout))
+	padding := "\r\n"
+	
+	payload := []byte(name + padding)
 	_, err = conn.Write(payload)
 	if err != nil {
 		return "", err
 	}
 
-	conn.SetReadDeadline(time.Now().Add(time.Second * 15))
+	conn.SetReadDeadline(time.Now().Add(timeout))
 	response, err := io.ReadAll(conn)
 	if err != nil {
 		return "", err
